9d39ab4cd36a5a30a03ca05f4525ddc835a577ab
emilioeiji <emilioeiji@gmail.com>
2023-03-14 05:40:22 +0900

    Incluindo botões de editar e excluir.

---
+++ src/data/user.js
@@ -0,0 +1,62 @@
+export default [
+    {
+        id: 1,
+        name: 'Emilio Eiji',
+        email: 'emilioeiji@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_1280.png'
+    },
+      {
+        id: 2,
+        name: 'Maria Silva',
+        email: 'mariasilva@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png'
+      },
+      {
+        id: 3,
+        name: 'João Santos',
+        email: 'joaosantos@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png'
+      },
+      {
+        id: 4,
+        name: 'Ana Oliveira',
+        email: 'anaoliveira@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2017/01/31/19/07/avatar-2026510_1280.png'
+      },
+      {
+        id: 5,
+        name: 'Pedro Sousa',
+        email: 'pedrosousa@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png'
+      },
+      {
+        id: 6,
+        name: 'Luiza Costa',
+        email: 'luizacosta@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366_960_720.png'
+      },
+      {
+        id: 7,
+        name: 'Carlos Silva',
+        email: 'carlossilva@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'
+      },
+      {
+        id: 8,
+        name: 'Julia Mendes',
+        email: 'juliamendes@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2014/03/24/17/19/teacher-295387_960_720.png'
+      },
+      {
+        id: 9,
+        name: 'Gabriel Pereira',
+        email: 'gabrielpereira@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_1280.png'
+      },
+      {
+        id: 10,
+        name: 'Mariana Costa',
+        email: 'marianacosta@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295774_1280.png'
+      }
+]
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,73 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View, Text, FlatList, Image, Alert } from 'react-native'
+import { ListItem } from 'react-native-elements'
+import { Button, Icon } from '@rneui/themed'
+import users from '../data/user'
 
 export default props => {
+
+    function confirmUserDeletion(user){
+        Alert.alert('Excluir Usuário', 'Deseja excluir o usuário ' + user.name + ' ?' , [
+            {
+                text: 'sim',
+                onPress(){
+                    console.warn('Delete' + user.name)
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
+
+    function getActions(user) {
+        return (
+            <>
+                <Button
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+                    type='clear'
+                    icon={<Icon name='edit' size={20} color='#1F7BF4' />}
+                />
+                <Button
+                    onPress={() => confirmUserDeletion(user)}
+                    type='clear'
+                    icon={<Icon name='delete' size={20} color='#f4511e' />}
+                />
+            </>
+        )
+    }
+
+    function getUserItem({ item: user }) {
+        return (
+            <ListItem 
+                key={user.id}
+                bottomDivider
+                onPress={() => props.navigation.navigate('UserForm', user)}
+            >
+                <View style={{ marginRight: 10 }}>
+                    <Image
+                    source={{ uri: user.avatarUrl }}
+                    style={{ width: 50, height: 50, borderRadius: 25 }}
+                    />
+                </View>
+                <View style={{ flex: 1 }}>
+                    <Text>{user.name}</Text>
+                    <Text>{user.email}</Text>
+                </View>
+                <View style={{flexDirection: 'row'}}>
+                    {getActions(user)}
+                </View>
+            </ListItem>
+        )
+    }
+
     return (
-        <Text>User List</Text>
+        <View>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            />
+        </View>
     )
 }
